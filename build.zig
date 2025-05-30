ef296e55a07150913ae177186151b1708b1887cb
Tristan Pemble <tpemble@gmail.com>
2025-05-30 16:23:21 -0700

    expose the module as public

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.createModule(.{
+    const mod = b.addModule("uuidz", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
